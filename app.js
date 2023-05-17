025260e6b26b59d29d30afe5f862e2d443d70cdf
LUIS HENRIQUE LEAL RIES <LHRIES@fspoa.br>
2023-05-16 21:45:27 -0300

    Criando as rotas iniciais

--- app.js
+++ app.js
@@ -2,8 +2,29 @@ const express = require('express');
 const app = express();
 const PORTA = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+//Recurso: Produtos
+
+app.get('/produtos', (req, res) => {
+    res.send("Listagem de Produtos");
+})
+
+app.get('/produtos/:id', (req,res) => {
+    const id = req.params.id;
+    res.send(`Buscar pelo id: ${id}`);
+})
+
+app.post('/produtos', (req, res) => {
+    res.send("Cadastrar Produto");
+})
+
+app.put('/produtos/:id', (req,res) => {
+    const id = req.params.id;
+    res.send(`Atualizar o produto com id: ${id}`);
+})
+
+app.delete('/produtos/:id', (req,res) => {
+    const id = req.params.id;
+    res.send(`Deletar o produto com id: ${id}`);
 })
 
 app.listen(PORTA, () => {
