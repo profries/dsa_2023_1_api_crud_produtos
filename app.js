6086398a16058dc99b2f6a8d008124e52f41f433
LUIS HENRIQUE LEAL RIES <LHRIES@fspoa.br>
2023-05-16 21:26:44 -0300

    Inicio do servidor

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORTA = 3000;
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(PORTA, () => {
+    console.log("Servidor iniciado com sucesso...")
+})
