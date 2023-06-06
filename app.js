399737437a3aed9cfe9bc38490b250dfa3788c3c
LUIS HENRIQUE LEAL RIES <LHRIES@fspoa.br>
2023-06-06 20:10:40 -0300

    Adicionando Routers e Controller

--- app.js
+++ app.js
@@ -1,63 +1,12 @@
 const express = require('express');
-const cadastroProdutos = require('./cadastro_produtos')
+const produtoRota = require('./rotas/produto_rotas')
+
 const app = express();
 const PORTA = 3000;
 
-const produtos = [{id:1, nome: "Produto 1", preco: 30}];
-
 app.use(express.json());
 
-//Recurso: Produtos
-app.get('/produtos', (req, res) => {
-    const listaProdutos = cadastroProdutos.listar();
-    res.json(listaProdutos);
-})
-
-app.get('/produtos/:id', (req,res) => {
-    const id = req.params.id;
-    try{
-        const produto = cadastroProdutos.buscarPorId(id);
-        res.json(produto);
-    } catch (err) {
-        res.status(err.numero).json(err);
-    }
-})
-
-app.post('/produtos', (req, res) => {
-    const produto = req.body;
-
-    try{
-        const produtoInserido = cadastroProdutos.inserir(produto);
-        res.status(201).json(produtoInserido);
-    }
-    catch (err) {
-        res.status(err.numero).json(err);
-    }
-})
-
-app.put('/produtos/:id', (req,res) => {
-    const id = req.params.id;
-    const produto = req.body;
-    try{
-        const produtoAtualizado = cadastroProdutos.atualizar(id,produto);
-        res.json(produtoAtualizado);
-    }
-    catch (err) {
-        res.status(err.numero).json(err);
-    }
-
-})
-
-app.delete('/produtos/:id', (req,res) => {
-    const id = req.params.id;
-    try{
-        const produtoDeletado = cadastroProdutos.deletar(id);
-        res.json(produtoDeletado);
-    }
-    catch (err) {
-        res.status(err.numero).json(err);
-    }
-})
+app.use('/produtos', produtoRota);
 
 app.listen(PORTA, () => {
     console.log("Servidor iniciado com sucesso...")
